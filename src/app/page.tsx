4e52fe4c9ed37fdd6ce02dca98cf1ce9a2947dfa
Cristhina <viszellechacon@gmail.com>
2025-11-09 20:54:38 -0500

    setting the conection between clerk and convex

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <SignedOut>
+        <SignUpButton />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
+}
