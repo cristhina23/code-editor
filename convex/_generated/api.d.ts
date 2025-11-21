769ad54ac9eedcb21816a8c36e351b11d0411cba
Cristhina <viszellechacon@gmail.com>
2025-11-20 22:32:04 -0500

    making progress with the code editor functionallity

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,14 +8,16 @@
  * @module
  */
 
-import type * as http from "../http.js";
-import type * as users from "../users.js";
-
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as codeExecutions from "../codeExecutions.js";
+import type * as http from "../http.js";
+import type * as lemonSqueezy from "../lemonSqueezy.js";
+import type * as snippets from "../snippets.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,18 +28,17 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  codeExecutions: typeof codeExecutions;
   http: typeof http;
+  lemonSqueezy: typeof lemonSqueezy;
+  snippets: typeof snippets;
   users: typeof users;
 }>;
-declare const fullApiWithMounts: typeof fullApi;
-
 export declare const api: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-export declare const components: {};
