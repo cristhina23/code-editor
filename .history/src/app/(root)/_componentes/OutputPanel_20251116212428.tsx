27a9aa7d0c439511ed0a5d86929138b0feeaad0a
Cristhina <viszellechacon@gmail.com>
2025-12-26 15:47:03 -0500

    chore: stop tracking .history folder

--- .history/src/app/(root)/_componentes/OutputPanel_20251116212428.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function OutputPanel() {
-  return (
-    <div>OutputPanel</div>
-  )
-}
--- .history/src/app/(root)/_componentes/OutputPanel_20251226143324.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import React from 'react'
-import { useCodeEditorStore } from '@/src/store/useCodeEditorStore';
-
-export default function OutputPanel() {
-  return (
-    <div>OutputPanel</div>
-  )
-}
